03d4038fe534b23e0fe21e9967feb0fd490fdbd8
ivans204 <ivansvrga204@gmail.com>
2021-07-15 15:49:46 +0200

    add useIntersectionObserver

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,21 @@
-import './App.css';
+import { useEffect, useRef } from 'react';
+import useIntersectionObserver from './hooks/useIntersectionObserver';
 
 function App() {
-    return <div className="App"></div>;
+    const naslov = useRef<HTMLHeadingElement>(null);
+
+    const entry = useIntersectionObserver(naslov, {});
+    console.log(entry);
+
+    useEffect(() => {
+        if (entry?.isIntersecting) console.log('use function');
+    }, [entry?.isIntersecting]);
+
+    return (
+        <div className="App">
+            <h1 ref={naslov}>naslov</h1>
+        </div>
+    );
 }
 
 export default App;

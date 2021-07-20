3be7e115178c582122609f6845e5ea44a241f5a6
ivans204 <ivansvrga204@gmail.com>
2021-07-20 15:20:16 +0200

    folder structure

--- src/components/PokemonInfo/index.tsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import './style.scss';
-
-const PokemonInfo = () => {
-    return <div className="pokemon-info-container">Info</div>;
-};
-
-export default PokemonInfo;
--- src/components/PokemonList/index.tsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import './style.scss';
-
-const PokemonList = () => {
-    return <div className="pokemon-list-container">List</div>;
-};
-
-export default PokemonList;

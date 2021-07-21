8f9147b29a6f97abb241e7d3f9cf965255e75e80
ivans204 <ivansvrga204@gmail.com>
2021-07-21 17:55:41 +0200

    add getPokemonData

--- src/api/api.ts
+++ src/api/api.ts
@@ -5,7 +5,11 @@ export const baseUrl = 'https://pokeapi.co/api/v2';
 const responseBody = (response: AxiosResponse) => response.data;
 
 export const getPokemons = async ({
-    pageParam = `${baseUrl}/pokemon?limit=100&offset=0`,
-}) => {
+    pageParam = `${baseUrl}/pokemon?limit=20&offset=0`,
+}): Promise<{ name: string; next: string; results: [] }> => {
     return await axios.get(pageParam).then(responseBody);
+};
+
+export const getPokemonData: any = async (pokemonName: string) => {
+    return await axios.get(`${baseUrl}/pokemon/${pokemonName}`);
 };

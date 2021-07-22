31be5bd6d6e863e441404c1ac05f2a58702f09d8
ivans204 <ivansvrga204@gmail.com>
2021-07-22 08:54:44 +0200

    imports cleanup

--- src/modules/components/PokemonInfo.tsx
+++ src/modules/components/PokemonInfo.tsx
@@ -1,8 +1,3 @@
-// import { useQuery } from 'react-query';
-
-// import { getPokemonData } from 'api';
-// import { useEffect } from 'react';
-
 const PokemonInfo = ({ data }: any) => {
     return <div className="pokemon-info-container">Info</div>;
 };

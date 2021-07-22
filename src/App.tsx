5d8ffa5fb3e2d498c5e96752e174347a011538b9
ivans204 <ivansvrga204@gmail.com>
2021-07-22 10:54:06 +0200

    change getPokemonData from App to PokemonInfo

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,17 @@
-import { useState, useEffect } from 'react';
+import { useState } from 'react';
 import 'App.scss';
-
-import { useQuery } from 'react-query';
 
 import { FlexItem } from 'core/components/FlexItem.style';
 import PokemonList from 'modules/components/PokemonList';
 import PokemonInfo from 'modules/components/PokemonInfo';
-import { getPokemonData } from 'api';
 
 function App() {
     const [pokemonName, setPokemonName] = useState<string>('');
 
-    const { data, refetch, isLoading } = useQuery(
-        ['pokemon', pokemonName],
-        () => getPokemonData(pokemonName),
-        {
-            enabled: !!pokemonName,
-        }
-    );
-
-    useEffect(() => {
-        refetch();
-        console.log(data);
-
-        //eslint-disable-next-line
-    }, [pokemonName]);
-
     return (
         <div className="container">
             <FlexItem borderRight>
-                {isLoading ? <h1>Loading...</h1> : <PokemonInfo data={data} />}
+                <PokemonInfo pokemonName={pokemonName} />
             </FlexItem>
             <FlexItem>
                 <PokemonList onSelect={setPokemonName} />
--- src/modules/components/PokemonInfo.tsx
+++ src/modules/components/PokemonInfo.tsx
@@ -1,5 +1,34 @@
-const PokemonInfo = ({ data }: any) => {
-    return <div className="pokemon-info-container">Info</div>;
+import { useEffect } from 'react';
+import { useQuery } from 'react-query';
+import { IPokemon } from 'modules/interfaces/interface.pokemon';
+
+import { getPokemonData } from 'api';
+
+const PokemonInfo = ({ pokemonName }: { pokemonName: string }) => {
+    const { data, refetch, isLoading } = useQuery<IPokemon>(
+        ['pokemon', pokemonName],
+        () => getPokemonData(pokemonName),
+        {
+            enabled: !!pokemonName,
+        }
+    );
+    const pokeData = { ...data?.data };
+
+    useEffect(() => {
+        refetch();
+        //eslint-disable-next-line
+    }, [pokemonName]);
+
+    if (!!!pokemonName) return <h1>No data... yet...</h1>;
+    if (isLoading) return <h1>Is Loading...</h1>;
+
+    return (
+        <div className="pokemon-info-container">
+            {pokeData?.name}
+            <br />
+            {pokeData?.height}
+        </div>
+    );
 };
 
 export default PokemonInfo;

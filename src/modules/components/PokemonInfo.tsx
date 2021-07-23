44565edfe0dec8d4b60a79c06e1373bc3db86873
ivans204 <ivansvrga204@gmail.com>
2021-07-23 15:37:12 +0200

    show stats on Info page

--- src/modules/components/PokemonInfo.tsx
+++ src/modules/components/PokemonInfo.tsx
@@ -46,16 +46,48 @@ const PokemonInfo = ({ pokemonName }: { pokemonName: string }) => {
 
     return (
         <div className="pokemon-info-container">
-            <Flex row>
-                <h3>#{pokeData.id}</h3>
-            </Flex>
+            <div className="info-section">
+                <Flex row>
+                    <h3>#{pokeData.id}</h3>
+                </Flex>
 
-            <Flex row>
-                <h1>{pokeData.name}</h1>
-            </Flex>
-            <Flex>
-                <img src={pokeData.sprites?.front_default} alt="" />
-            </Flex>
+                <Flex row>
+                    <h1>{pokeData.name}</h1>
+                </Flex>
+                <Flex>
+                    <img src={pokeData.sprites?.front_default} alt="" />
+                </Flex>
+            </div>
+            <div className="info-section">
+                <h2 style={{ borderBottom: '1px solid #e8ecef' }}>
+                    Base Stats
+                </h2>
+                <Flex row>
+                    <Flex column align="center" style={{ marginRight: '10px' }}>
+                        Height
+                        <span>{pokeData.height}</span>
+                    </Flex>
+                    <Flex column align="center" style={{ marginRight: '10px' }}>
+                        Weight <span>{pokeData.weight}</span>
+                    </Flex>
+                </Flex>
+            </div>
+            <div className="info-section">
+                <h2 style={{ borderBottom: '1px solid #e8ecef' }}>Stats</h2>
+                <Flex row wrap>
+                    {pokeData.stats?.map((stat) => (
+                        <Flex
+                            key={stat.stat.name}
+                            column
+                            align="center"
+                            style={{ flex: '0 1 25%' }}
+                        >
+                            {stat.stat.name}
+                            <span>{stat.base_stat}</span>
+                        </Flex>
+                    ))}
+                </Flex>
+            </div>
         </div>
     );
 };

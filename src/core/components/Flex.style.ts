1f60df7e9a1536ba7b8d0467f5c4b84857b2b44f
ivans204 <ivansvrga204@gmail.com>
2021-07-23 15:36:52 +0200

    add wrap to Flex component

--- src/core/components/Flex.style.ts
+++ src/core/components/Flex.style.ts
@@ -1,21 +1,18 @@
 import styled from 'styled-components';
 
 export interface FlexProps {
-    flex?: number;
-    flexStart?: boolean;
-    flexEnd?: boolean;
-    space?: boolean;
-    grow?: number;
     column?: boolean;
     row?: boolean;
     justify?: string;
     align?: string;
+    wrap?: boolean;
 }
 
 export const Flex = styled.div<FlexProps>`
     display: flex;
     ${({ row }) => row && `flex-direction: row;`}
     ${({ column }) => column && `flex-direction: column;`}
+    ${({ wrap }) => wrap && `flex-wrap: wrap;`}
     ${({ justify }) => justify && `justify-content: ${justify};`} 
     ${({ align }) => align && `align-items: ${align};`}
 `;
